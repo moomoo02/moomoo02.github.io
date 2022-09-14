61c22ea3ffc4e6c546e18fa0d1e82d4df68ff772
vince nguyen <vincehnguyen@ucsb.edu>
2022-09-13 23:54:26 -0700

    Added shiba

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+import SceneInit from './lib/SceneInit';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
+
+    // const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
+
+    let loadedModel;
+    const glftLoader = new GLTFLoader();
+    glftLoader.load('./assets/shiba/scene.gltf', (gltfScene) => {
+      loadedModel = gltfScene;
+      console.log(loadedModel);
+
+      // gltfScene.scene.rotation.y = Math.PI / 8;
+      // gltfScene.scene.position.y = 3;
+      // gltfScene.scene.scale.set(10, 10, 10);
+      test.scene.add(gltfScene.scene);
+    });
+
+    // const animate = () => {
+    //   if (loadedModel) {
+    //     loadedModel.scene.rotation.x += 0.01;
+    //     loadedModel.scene.rotation.y += 0.01;
+    //     loadedModel.scene.rotation.z += 0.01;
+    //   }
+    //   requestAnimationFrame(animate);
+    // };
+    // animate();
+  }, []);
 
   return (
-    <div className="App">
-      <div class="font-bold text-6xl">Hello World</div>
+    <div>
+      <div>Vince Nguyen</div>
+      <canvas id="myThreeJsCanvas" />
+      {/* <p>hi</p> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
